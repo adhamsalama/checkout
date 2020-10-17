51e2d5211a0ae519ba6b0e71e24ceb63507c253d
adhamsalama <adhamsalama666@gmail.com>
2020-10-17 10:55:11 +0200

    Removed commented code

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,13 +3,6 @@ from .models import Item
 from django import forms
 from datetime import datetime
 
-#class ItemForm(ModelForm):
-#    class Meta:
-#        model = Item
-#        fields = ["name", "price", "quantity", "category", "date", "comment"]
-#    #category = forms.CharField(required=False)
-#    #date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', "value": datetime.now().strftime('%Y-%m-%d')}))
-#    field_order=["name", "price", "quantity", "date", "category", "comment"]
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -23,4 +16,4 @@ class ItemForm(forms.ModelForm):
             'date': DateInput(),
         }
     category = forms.CharField(required=False)
-    field_order=["name", "price", "quantity", "seller", "date", "category", "comment"]
+    field_order = ["name", "price", "quantity", "seller", "date", "category", "comment"]
