f85dfa7cf6e58b9771515b8b1123f68869ded38e
adhamsalama <adhamsalama666@gmail.com>
2020-10-17 14:10:57 +0200

    Changed date input type

--- payment/forms.py
+++ payment/forms.py
@@ -1,10 +1,16 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Payment
 
-class PaymentForm(ModelForm):
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = '__all__'
         exclude = ('user', )
+        widgets = {
+            'date': DateInput(),
+        }
 
     field_order = ["value", "source", "date"]
