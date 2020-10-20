931ee37fd2092dc2dab3129b9cb732cdc1aafd49
adhamsalama <adhamsalama666@gmail.com>
2020-10-21 01:03:54 +0200

    Added code from heroku

--- final_project/settings.py
+++ final_project/settings.py
@@ -127,3 +127,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
