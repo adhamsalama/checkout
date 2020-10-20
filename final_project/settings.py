1837fad2ed91f93028512fa984088a74de1917e4
adhamsalama <adhamsalama666@gmail.com>
2020-10-21 01:11:47 +0200

    Removed code from heroku

--- final_project/settings.py
+++ final_project/settings.py
@@ -127,8 +127,3 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
