76a614e9a248a8f674138c88750898d31eb51d3b
adhamsalama <adhamsalama666@gmail.com>
2020-10-22 21:05:10 +0200

    Used environment variable for secret key

--- final_project/settings.py
+++ final_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fps$yqm*939i&)v3nht=oeq8hfhw^+8ye^c#wv#k2*#bxiam6)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
